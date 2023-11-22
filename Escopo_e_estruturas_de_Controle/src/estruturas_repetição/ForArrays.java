710a95b08c2158587ae3e655d3ab2cb04ad85ed5
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-11-22 14:28:31 -0300

    Commit do estudos em Estruturas de Repetição

--- Escopo_e_estruturas_de_Controle/src/estruturas_repetição/ForArrays.java
+++ Escopo_e_estruturas_de_Controle/src/estruturas_repetição/ForArrays.java
@@ -1,5 +1,11 @@
 package estruturas_repetição;
 
 public class ForArrays {
-
+	public static void main(String[] args) {
+		String comida[] = {"PÃO", "ALFACE", "CARNE", "PÃO"};
+		for(int c = 0;c < 4;c++) {
+			System.out.println(comida[c]+" ");
+		}
+		System.out.println("Assim que se faz um Hambureger!");
+	}
 }
