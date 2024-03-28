c3e7bf477499e92b4964f20fd17d76a65eae60c9
Pedro Henrique Costa Santiago <pedro.santiago8051@outlook.com>
2024-03-28 09:03:59 -0300

    Add files via upload

---
+++ Escopo_e_estruturas_de_Controle/src/desafio_SistemaValidação/Desafio_Contador.java
@@ -0,0 +1,61 @@
+package desafio_SistemaValidação;
+import java.util.Scanner;
+
+class ParametrosInvalidosException extends Exception {
+    /**
+	 * PARA CRIRAR UMA EXCEÇÃO PRECISA CRIAR UMA CLASSE TAMBEEEEM!!!!
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
+public class Desafio_Contador {
+	static void contadorParametros(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois) {
+			//Demonstra um metodo simples de mostrar a mensagem de erro como a padrão do java
+			throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+			
+			//Imprime uma mensagem padrão !!
+			//System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}else {
+			int parametroNovo = parametroDois - parametroUm;
+			for (int i=1;i <= parametroNovo;i++) {
+				int parametroAux = parametroUm + i;
+				System.out.println("Imprimindo o "+i+"º Número: "+(parametroAux));
+			}
+		}
+		
+	} 
+	
+	public static void main(String[] args) {
+		
+		
+		try (Scanner terminal = new Scanner(System.in)) {
+			System.out.println("Digite o primeiro parâmetro: ");
+			int parametroUm = terminal.nextInt();
+			System.out.println("Digite o segundo parâmetro: ");
+			int parametroDois = terminal.nextInt();
+			try {
+				contadorParametros(parametroUm, parametroDois);
+				
+			}catch(ParametrosInvalidosException exception) {
+				//Chama a mensagem de erro da exceção ParametrosInvalidosException do metodo contadorParametros();
+				exception.printStackTrace();
+				
+				
+				//Aind posso usar o Código abaixo, personalizando ainda mais a minha Exception !!!!
+				//String mensagem = exception.getMessage();
+				//System.out.println("Ocorreu uma exceção: "+ mensagem);
+				
+			}
+		}
+		
+		
+		
+	}
+	
+	
+
+}
